303847d95938ff009e3a1dc3e931f15afce188a5
Cancan <felicityzhangcc@163.com>
2026-03-19 03:39:43 +0800

    feat(All): Start 方法的意义在于提供回调
    Start 方法提供回调函数

--- server.go
+++ server.go
@@ -1,6 +1,9 @@
 package gws
 
-import "net/http"
+import (
+	"net"
+	"net/http"
+)
 
 type Server interface {
 	http.Handler
@@ -20,6 +23,13 @@ func (h *HTTPServer) ServeHTTP(writer http.ResponseWriter, request *http.Request
 }
 
 func (h *HTTPServer) Start(addr string) error {
-	//TODO implement me
-	panic("implement me")
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	// 在这里，可以让用户注册 after start 回调
+	// 比如在这里执行一些你业务所需的前置条件，比如连接数据库，或者加载一些配置文件等
+
+	return http.Serve(l, nil)
 }
