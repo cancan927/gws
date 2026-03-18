ac92276a78e1118307cb1dc79e487bf1fcde9d34
Cancan <felicityzhangcc@163.com>
2026-03-19 03:29:17 +0800

    feat(All): Server 的定义，版本 v2
    组合 http.Handler并且增加 Start方法，此外，Server 提供HTTPS 的支持的实现类

--- server.go
+++ server.go
@@ -4,4 +4,22 @@ import "net/http"
 
 type Server interface {
 	http.Handler
+	Start(addr string) error
+}
+
+type HTTPServer struct {
+}
+
+type HTTPSServer struct {
+	HTTPServer
+}
+
+func (h *HTTPServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (h *HTTPServer) Start(addr string) error {
+	//TODO implement me
+	panic("implement me")
 }
--- server_test.go
+++ server_test.go
@@ -7,6 +7,10 @@ import (
 
 func TestServer(t *testing.T) {
 	var s Server
+	// 用户依然可以直接使用 http.ListenAndServe 和 http.ListenAndServeTLS 来启动服务器
 	http.ListenAndServe("8080", s)
 	http.ListenAndServeTLS("8081", "cert.pem", "key.pem", s)
+
+	// 也可以使用自定义的 Start 方法来启动
+	s.Start("8080")
 }
